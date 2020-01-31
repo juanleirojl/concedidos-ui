fb54ececc0572abdf3b53ecd3a185d41e990e314
Juan Leiro <juan.leiro@gmail.com>
2020-01-31 19:01:35 -0300

    Commit inicial- Portal com AdminLTE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'concedidos-ui';
+
+  teste = 'Juan Leiro' ;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './core/header/header.component';
+import { MenuLateralComponent } from './core/menu-lateral/menu-lateral.component';
+import { MenuConfigComponent } from './core/menu-config/menu-config.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeaderComponent,
+    MenuLateralComponent,
+    MenuConfigComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
